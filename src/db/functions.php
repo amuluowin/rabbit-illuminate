149c4ca07ca27407c45817d92b37c90a8d28bb79
albert <63851587@qq.com>
2018-10-30 09:29:27 +0800

    add function

---
+++ src/db/functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/30
+ * Time: 9:25
+ */
+
+if (!function_exists('dbRelease')) {
+    function dbRelease(\Illuminate\Support\Collection $collection = null): \Illuminate\Support\Collection
+    {
+        /** @var Manager $db */
+        $db = \rabbit\core\ObjectFactory::get('db');
+        $db->release();
+        return $collection;
+    }
+}
