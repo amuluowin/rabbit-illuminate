727ae3f5e436cfa7b0f01bcf2682ed0ee3134639
albert <63851587@qq.com>
2018-11-06 20:12:26 +0800

    update

--- src/db/functions.php
+++ src/db/functions.php
@@ -6,12 +6,11 @@
  * Time: 9:25
  */
 
-if (!function_exists('dbRelease')) {
-    function DbRelease(\Illuminate\Support\Collection $collection = null): ?\Illuminate\Support\Collection
+if (!function_exists('DbRelease')) {
+    function DbRelease()
     {
         /** @var Manager $db */
         $db = \rabbit\core\ObjectFactory::get('db');
         $db->release();
-        return $collection;
     }
 }
